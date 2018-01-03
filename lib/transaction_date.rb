3d33fff29d67fd77d83e665f447724a67ad317cc
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-03 11:06:48 +0000

    transaction_date_spec.rb checks for letters in date

--- lib/transaction_date.rb
+++ lib/transaction_date.rb
@@ -5,9 +5,25 @@ class TransactionDate
   def initialize; end
 
   def new_date(date)
+    check_parse_date(date)
+    date_validations(date)
+    parse_date(date)
+  end
+
+  private
+
+  def parse_date(date)
     Date.parse(date)
-    rescue ArgumentError
-      raise "Deposit rejected: incorrect date, please format as DD/MM/YYYY"
-    date
+  end
+
+  def check_parse_date(date)
+    Date.parse(date)
+  rescue ArgumentError
+    raise 'Deposit rejected: incorrect date, please format as DD/MM/YYYY'
+  end
+
+  def date_validations(date)
+    raise 'Deposit rejected: cannot use letters in date' if
+      date.chars.any? { |char| ('a'..'z').cover? char.downcase }
   end
 end
--- spec/transaction_date_spec.rb
+++ spec/transaction_date_spec.rb
@@ -8,9 +8,24 @@ describe 'TransactionDate:' do
       expect(@transaction_date.new_date('10/01/2012')).to be_instance_of(Date)
     end
 
-    it 'will throw error for incorrect day entry' do
-      message = "Deposit rejected: incorrect date, please format as DD/MM/YYYY"
-      expect { @transaction_date.new_date('101/01/2012') }.to raise_error(message)
+    it 'will throw error for incorrect date entry' do
+      error = 'Deposit rejected: incorrect date, please format as DD/MM/YYYY'
+      expect { @transaction_date.new_date('101/01/2012') }.to raise_error(error)
+      expect { @transaction_date.new_date('32/01/2012') }.to raise_error(error)
+    end
+
+    it 'will throw error for incorrect month entry' do
+      error = 'Deposit rejected: incorrect date, please format as DD/MM/YYYY'
+      expect { @transaction_date.new_date('10/011/2012') }.to raise_error(error)
+      expect { @transaction_date.new_date('10/13/2012') }.to raise_error(error)
+    end
+
+    it 'will throw error for letters added in date' do
+      error = 'Deposit rejected: cannot use letters in date'
+      expect { @transaction_date.new_date('DD/01/2012') }.to raise_error(error)
+      expect { @transaction_date.new_date('10/MM/2012') }.to raise_error(error)
+      expect { @transaction_date.new_date('10/01/YYYY') }.to raise_error(error)
+      expect { @transaction_date.new_date('10/01/2o12') }.to raise_error(error)
     end
   end
 end
